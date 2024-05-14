6323814c597c1091c68498bbe6424f4764b43adb
nicothebrush <nicola.riolini@gmail.com>
2024-05-14 11:02:24 +0200

    generic bugfix (fast message)

--- electrical_account_stats/account_stat.py
+++ electrical_account_stats/account_stat.py
@@ -232,7 +232,7 @@ class AccountAnalyticAccount(orm.Model):
                 'Costo', 'Ricavo', 'Utile', 'Errore'
                 ]
             width = [
-                14, 12, 32, 10,
+                14, 18, 40, 10,
                 8, 16,
                 10, 10, 10, 5,
             ]
